bc19f34acb37562b0e8de9a7f4b411f39ca58aac
Thomas <guntho.neo@gmail.com>
2021-09-13 12:27:21 +0200

    Adding configuration files from project

--- noxfile.py
+++ noxfile.py
@@ -3,9 +3,14 @@ import os
 
 # NOX USAGE: https://nox.thea.codes/en/stable/usage.html
 
-WORKING_DIR = os.path.dirname(__file__)
-CONFIG_TOML = os.path.join(WORKING_DIR, "pyproject.toml")
-FILE = os.path.join(WORKING_DIR, "src", "debug_utils.py")
+ROOT_DIR = os.path.dirname(__file__)
+CONFIG_TOOLS_DIR = os.path.join(ROOT_DIR, ".config_tools")
+CONFIG_TOML = os.path.join(CONFIG_TOOLS_DIR, "pyproject.toml")
+FLAKE_TOML = os.path.join(CONFIG_TOOLS_DIR, "flake8.toml")
+
+SRC_DIR = os.path.join(ROOT_DIR, "src")
+FILE = os.path.join(SRC_DIR, "classes.py")
+TARGET = SRC_DIR
 
 # Stopping if any session fails
 nox.options.stop_on_first_error = True
@@ -13,26 +18,37 @@ nox.options.stop_on_first_error = True
 # Re-using virtualenvs
 nox.options.reuse_existing_virtualenvs = True
 
+# provide a set of sessions that are run by default:
+# nox.options.sessions = ["isort", "black_diff_only", "flake"]
+nox.options.sessions = ["isort", "black", "flake"]
+
 
 @nox.session(python="3.8")
 def isort(session):
+    # https://pycqa.github.io/isort/docs/upgrade_guides/5.0.0.html
     session.install("isort")
-    session.run("isort", "--settings-path", CONFIG_TOML, "-d", "-v", FILE)
+    session.run("isort", "--settings-path", CONFIG_TOML, TARGET)
 
 
 @nox.session(python="3.8")
 def mypy(session):
     session.install("mypy")
-    session.run("mypy", "--config-file", CONFIG_TOML, FILE)
+    session.run("mypy", "--config-file", CONFIG_TOML, TARGET)
+
+
+@nox.session(python="3.8")
+def black_diff_only(session):
+    session.install("black")
+    session.run("black", "--config", CONFIG_TOML, "--diff", TARGET)
 
 
 @nox.session(python="3.8")
 def black(session):
     session.install("black")
-    session.run("black", "--config", CONFIG_TOML, "--diff", FILE)
+    session.run("black", "--config", CONFIG_TOML, TARGET)
 
 
 @nox.session(python="3.8")
 def flake(session):
     session.install("flake8")
-    session.run("flake8", "--config", CONFIG_TOML, FILE)
+    session.run("flake8", "--config", FLAKE_TOML, TARGET)
